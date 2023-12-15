2964aeb7888ac41905b9e5ceb68f34f54bf4eb83
thiago <104877209+thiago3030@users.noreply.github.com>
2023-12-15 01:13:28 -0300

    Add files via upload

---
+++ Inicio/comentario.js
@@ -0,0 +1,12 @@
+// console.log e usado para exibir algo na tela do computador!
+console.log('passo #1');
+console.log('passo #2');
+console.log('passo #3');
+
+
+/*
+* Esse e um 
+* comentario
+* de multiplas
+* linhas!
+*/
---
+++ Inicio/dados_1.js
@@ -0,0 +1,16 @@
+var nome = "caneta";
+var quantida = 10;
+var preco = 6.4;
+var imposto = 1.5;
+var precoFinal = preco + imposto;
+
+console.log(nome);
+console.log("caneta");
+console.log(quantida);
+console.log(preco);
+console.log(imposto);
+console.log(precoFinal);
+
+nome = "Caneta BIC";
+console.log(nome);
+
